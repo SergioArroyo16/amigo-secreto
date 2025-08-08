3fb5747051b36cfceec419d3cec850955c2412ef
Sergio <soldierlong94@gmail.com>
2025-08-08 01:46:41 -0600

    Registrar nombre ingresados y guardarlos en un arreglo

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//VARIABLES GLOBALES
+let amigos = [];
+
+
+//FUNCIONES INICIALES
+limpiarCampo();
+
+//FUNCIONES GLOBALES
+function limpiarCampo(){
+    document.querySelector('#amigo').value = '';
+}
+
+function insertarNombre(){
+    let elemento = document.querySelector('li');
+    elemento.innerHTML();
+}
+
+//AGREGAR NOMBRES AL ARREGLO
+function agregarAmigo(){
+    let nombreAgregado = document.getElementById('amigo').value;
+    
+    if (nombreAgregado=="") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(nombreAgregado);
+    }
+    console.log(amigos);
+    limpiarCampo();
+    
+}
+
+
+
+for (let i = 0; i < amigos.length; i++) {
+    let elemento = document.querySelector('li');
+    elemento.innerHTML(amigos[i]);
+}
