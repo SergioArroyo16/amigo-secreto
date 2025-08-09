89dc3c54e29f7842c6fe5bc2cfef6c1faf5dd308
Sergio <soldierlong94@gmail.com>
2025-08-09 02:34:35 -0600

    Actualización de app.js e index.html

--- app.js
+++ app.js
@@ -1,37 +1,73 @@
 //VARIABLES GLOBALES
 let amigos = [];
 
-
 //FUNCIONES INICIALES
 limpiarCampo();
+limpiarLista();
 
 //FUNCIONES GLOBALES
-function limpiarCampo(){
+function limpiarCampo() {
     document.querySelector('#amigo').value = '';
 }
 
-function insertarNombre(){
-    let elemento = document.querySelector('li');
-    elemento.innerHTML();
+function limpiarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
 }
 
-//AGREGAR NOMBRES AL ARREGLO
-function agregarAmigo(){
+function limpiarResultado() {
+    let lista = document.getElementById('resultado');
+    lista.innerHTML = "";
+}
+
+//AGREGAR NOMBRES AL ARREGLO Y VERIFICAR SÍ EL CAMPO ESTÁ VACÍO
+function agregarAmigo() {
+
     let nombreAgregado = document.getElementById('amigo').value;
-    
-    if (nombreAgregado=="") {
+
+    if (nombreAgregado == "") {
         alert("Por favor, inserte un nombre");
     } else {
         amigos.push(nombreAgregado);
+        imprimirLista(nombreAgregado);
     }
-    console.log(amigos);
-    limpiarCampo();
     
+    limpiarCampo();
+    limpiarResultado()
+}
+
+//MOSTRAR LA LISTA DE AMIGOS CONFORME SE VAYAN INGRESANDO LOS NOMBRES
+function imprimirLista() {
+
+    let elemento = document.getElementById('listaAmigos');
+    let listaAmigos = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos = listaAmigos + `<li>${amigos[i]}</li>`;
+    }
+
+    elemento.innerHTML = listaAmigos;
 }
 
 
+//SORTEAR EL NOMBRE DEL AMIGO SECRETO Y MOSTRARLO
+function sortearAmigo() {
 
-for (let i = 0; i < amigos.length; i++) {
-    let elemento = document.querySelector('li');
-    elemento.innerHTML(amigos[i]);
+    let elemento = document.getElementById('resultado');
+
+    if (amigos.length == 0) {
+
+        elemento.innerHTML = `No has ingresado ningún nombre.\nInténtalo de nuevo, por favor.`;
+
+    } else {
+
+        let numeroMax = amigos.length;
+        let numeroSorteado = Math.floor(Math.random() * numeroMax) + 1;
+
+        limpiarLista();
+        elemento.innerHTML = `El amigo secreto sorteado es: ${amigos[numeroSorteado - 1]}`;
+
+    }
+
 }
+
